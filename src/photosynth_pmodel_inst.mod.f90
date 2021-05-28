390bbb1aeeff06c0e3f461988551d91327a553db
padasch <96p.schneider@gmail.com>
2021-05-28 17:03:13 +0200

    Corrected calc_ftemp_inst_vcmax formulation and call

--- src/photosynth_pmodel_inst.mod.f90
+++ src/photosynth_pmodel_inst.mod.f90
@@ -1,7 +1,6 @@
 module md_photosynth_inst
 
-  use md_photosynth, only: pmodel, zero_pmodel, outtype_pmodel, calc_ftemp_inst_vcmax, calc_ftemp_inst_jmax, &
-                           co2_to_ca, calc_gammastar, calc_kmm
+  use md_photosynth, only: pmodel, zero_pmodel, outtype_pmodel, calc_ftemp_inst_vcmax, calc_ftemp_inst_jmax, co2_to_ca, calc_gammastar, calc_kmm
   
   implicit none 
   
@@ -22,12 +21,16 @@ module md_photosynth_inst
       ! real :: ci_j
       ! real :: ci_c
       ! real :: gpp
+
+      ! TODO: For debugging
+      real :: term
+
   end type outtype_pmodel_inst
   
   
   contains
     
-    function pmodel_inst(vcmax25, jmax25, xi, tc, vpd, co2, fapar, ppfd, patm, kphio, tc_home, fixedCi) result(out_pmodel_inst)
+    function pmodel_inst(vcmax25, jmax25, xi, tc_leaf, vpd, co2, fapar, ppfd, patm, kphio, tc_growth, tc_home, fixedCi) result(out_pmodel_inst)
       
       !--------------------------------!
       ! Definition of variables        !
@@ -37,13 +40,14 @@ module md_photosynth_inst
       real, intent(in) :: vcmax25
       real, intent(in) :: jmax25
       real, intent(in) :: xi
-      real, intent(in) :: tc
+      real, intent(in) :: tc_leaf
       real, intent(in) :: vpd
       real, intent(in) :: co2
       real, intent(in) :: fapar
       real, intent(in) :: ppfd
       real, intent(in) :: patm
       real, intent(in) :: kphio
+      real, intent(in) :: tc_growth
       real, intent(in) :: tc_home
       logical, intent(in) :: fixedCi
   
@@ -64,7 +68,13 @@ module md_photosynth_inst
       real :: gs_c
       real :: assim
       real :: anet
+      real :: rd_to_vcmax
       real :: rd
+      real :: rd_25
+      real :: q10
+
+      ! TODO: For debugging
+      real :: term
   
       ! Output variables
       type(outtype_pmodel_inst) :: out_pmodel_inst
@@ -77,14 +87,17 @@ module md_photosynth_inst
       ca = co2_to_ca(co2, patm)
       
       ! Instantaneous gammastar
-      gammastar = calc_gammastar(tc, patm)
+      gammastar = calc_gammastar(tc_leaf, patm)
       
       ! Instantaneous K
-      kmm = calc_kmm(tc, patm)
+      kmm = calc_kmm(tc_leaf, patm)
         
       ! Instantaneous vcmax and jmax
-      vcmax = calc_ftemp_inst_vcmax(tc, tc, tcref = 25.0 ) * vcmax25
-      jmax  = calc_ftemp_inst_jmax(tc, tc, tc_home, tcref = 25.0 ) * jmax25
+      ! calc_ftemp_inst_vcmax( tcleaf, tcgrowth, tcref )
+      ! calc_ftemp_inst_jmax( tcleaf, tcgrowth, tc_home, tcref )
+
+      vcmax = calc_ftemp_inst_vcmax(tc_leaf, tc_growth, tcref = 25.0 ) * vcmax25
+      jmax  = calc_ftemp_inst_jmax(tc_leaf, tc_growth, tc_home, tcref = 25.0 ) * jmax25
         
       ! Instantaneous kphio - TODO: Not needed because kphio does not adapt on daily changes?
       ! kphio = kphio * calc_ftemp_kphio()
@@ -102,7 +115,6 @@ module md_photosynth_inst
       a_j = L * kphio * ppfd * (ci_j - gammastar)/(ci_j + 2 * gammastar)
       gs_j = a_j / kv
       
-      ! If check for fixedCi
       ! Ac, gs free
       ci_c = ci_j
       a_c = vcmax * (ci_c - gammastar)/(ci_c + kmm)
@@ -115,10 +127,8 @@ module md_photosynth_inst
       ci = max(ci_c, ci_j)
 
       ! Dark respiration
-      ! TODO: add rd_to_vcmax as variable
-      ! Ratio of Rdark to Vcmax25, number from Atkin et al., 2015 for C3 herbaceous
-      
-      rd = vcmax25 * 0.01400000 * calc_ftemp_inst_rd(tc)
+      rd_to_vcmax = 0.015                   ! Ratio of Rdark to Vcmax25, number from Atkin et al., 2015 for C3 herbaceous TODO: rd_to_vcmax clean-up
+      rd = vcmax25 * rd_to_vcmax * calc_ftemp_inst_rd(tc_leaf)
 
       ! Net Assimilation
       anet = assim * (1.0 - gammastar/ci) - rd
@@ -126,14 +136,18 @@ module md_photosynth_inst
       !--------------------------------!
       ! Definition of output           !
       !--------------------------------!
-      out_pmodel_inst%assim = assim
       out_pmodel_inst%anet  = anet
       out_pmodel_inst%rd    = rd
       out_pmodel_inst%vcmax = vcmax
       out_pmodel_inst%jmax  = jmax
       out_pmodel_inst%gammastar  = gammastar
       out_pmodel_inst%ci    = ci
-  
+
+      ! TODO: For debugging
+      out_pmodel_inst%assim = assim
+      out_pmodel_inst%rd    = rd
+      out_pmodel_inst%term  = (1.0 - gammastar/ci)
+
     end function pmodel_inst
 
     ! TODO: Clean up where calc_ftemp_inst_rd() is located and called!
